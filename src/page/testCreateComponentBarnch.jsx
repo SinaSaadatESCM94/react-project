0494bfbdf33c397beb4d48a1d35be09208ebdace
escm94 <escm94.sinasaadat@gmail.com>
2022-12-30 10:13:41 +0330

    TestCreateComponentBarnch: create a test page for create-component branch and its sub branches

---
+++ src/page/testCreateComponentBarnch.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+// importing font awsome
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+// importing svg-components
+import { MessageIcon } from "../content/svg-component";
+// importing components
+import ApiTest from "../component/apiTest/ApiTest";
+import ContentTable from "../component/ContentTable/ContentTable";
+import Input from "../component/input/Input";
+import Button from "../component/button/Button";
+
+export default function TestCreateComponentBarnch() {
+	// states
+	const [email, setEmail] = useState({
+		value: "",
+		hasError: true,
+		touched: false,
+	});
+	// const [ users, setUsers ] = useState([]);
+	// const url = [
+	// 	"https://reqres.in/api/users?delay=3",
+	// 	"https://reqres.in/api/users?page=1",
+	// 	"https://reqres.in/api/users?page=2",
+	// 	"https://reqres.in/api/unknown",
+	// ];
+	// Functions
+	// ApiTest(url[0], setUsers);
+	// DOM
+	return (
+		<main className="w-100 d-flex flex-column align-items-center justify-content-center mt-5">
+			{/* <ContentTable dataArray={users} /> */}
+			<form className="d-flex flex-column align-items-center justify-content-between col-6">
+				<fieldset className="w-100 d-flex align-items-center justify-content-center mb-4">
+					<Input
+						label={"email"}
+						labelFontSize="h4"
+						fontSize="h5 m-0"
+						labelColor="text-muted"
+						background="#adb5bd"
+						border="border border-secondary"
+						borderRadius="rounded-4"
+						icon={faEnvelope}
+						// icon={<MessageIcon size={12} color="red" />}
+						inputIconColor="text-primary"
+						inputColor="text-muted"
+						placeholder="abc@mail.com"
+						type="email"
+						id="email"
+						name="email"
+						value={email}
+						setValue={setEmail}
+						regEx=""
+					/>
+				</fieldset>
+				<fieldset className="w-100 d-flex align-items-center justify-content-center mb-4">
+					<Button
+						type="button"
+						id="button"
+						value="button"
+						disabled={false}
+						form="form"
+						// className=""
+						// backgroundColor=""
+						// border=""
+						// borderRadius=""
+						// paddingY=""
+						// fontSize=""
+						// disableColor=""
+						// inableColor=""
+						showLoadingOnClick={true}
+						onClick={() => {
+							console.log("submit");
+						}}
+						label="submit"
+					/>
+				</fieldset>
+			</form>
+		</main>
+	);
+}
