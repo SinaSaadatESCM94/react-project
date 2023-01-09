01721b606db5a500ec8b558ecdd3b650da7fbd78
escm94 <escm94.sinasaadat@gmail.com>
2023-01-10 00:04:53 +0330

    Logo.jsx: creating component

---
+++ src/component/logo/Logo.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Logo.scss";
+
+function Logo({ variant }) {
+	return (
+		<div
+			id="logo"
+			className={`position-relative d-flex align-items-center justify-content-center ${variant}`}
+		>
+			<span
+				id="circle-logo"
+				className="position-absolute w-100 h-100 rounded-circle bg-purple"
+			></span>
+			<span
+				id="fade-circle-logo"
+				className="position-absolute h-100 top-100 translate-middle-y rounded-1"
+			></span>
+		</div>
+	);
+}
+
+Logo.propTypes = {
+	variant: PropTypes.string,
+};
+Logo.defaultProps = {
+	variant: "normal",
+};
+
+export default Logo;
