4446d34444c36789e5c0dc12364201bb0c7a33c4
escm94 <escm94.sinasaadat@gmail.com>
2023-01-20 13:12:38 +0330

    index: initialize router for ptoject

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
+import Layout from "./layout/layout";
+import Login from "./page/login/login";
+import SignUp from "./page/sign-up/signUp";
+import TestCreateComponentBarnch from "./page/testCreateComponentBarnch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<BrowserRouter>
+		<Routes>
+			<Route
+				path="/SinaSaadatESCM94/react-project.git"
+				element={<Layout />}>
+				<Route
+					index
+					element={<Login />}
+				/>
+				<Route
+					path="signup"
+					element={<SignUp />}
+				/>
+				<Route
+					path="test-component"
+					element={<TestCreateComponentBarnch />}
+				/>
+			</Route>
+		</Routes>
+	</BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
