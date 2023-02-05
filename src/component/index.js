879d3136ba3505fd1ad77bbebcff234dd0b13252
escm94 <escm94.sinasaadat@gmail.com>
2023-02-05 21:34:55 +0330

    index: add components

--- src/component/index.js
+++ src/component/index.js
@@ -6,5 +6,34 @@ import Input from "./input/Input";
 import LoadingButton from "./loading/LoadingButton";
 import Logo from "./logo/Logo";
 import MenuIcon from "./menu-icon/menuIcon";
-export { ApiTest, Button, ContentTable, Input, LoadingButton, Logo, MenuIcon };
-export * from "./specific";
+import BoardHead from "./boardHead";
+import Calender from "./calender";
+import Card from "./card";
+import CardContainer from "./cardContainer";
+import Contributer from "./contributer";
+import DrivePoster from "./drivePoster";
+import Header from "./header";
+import Note from "./note";
+import Shortcut from "./shortcut";
+import SideBar from "./sideBar";
+import TabBar from "./tabBar";
+export {
+  ApiTest,
+  Button,
+  ContentTable,
+  Input,
+  LoadingButton,
+  Logo,
+  MenuIcon,
+  BoardHead,
+  Calender,
+  Card,
+  CardContainer,
+  Contributer,
+  DrivePoster,
+  Header,
+  Note,
+  Shortcut,
+  SideBar,
+  TabBar,
+};
