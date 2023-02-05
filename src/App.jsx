d7d47db8b15566c4a04677ea4daf163f47109fba
escm94 <escm94.sinasaadat@gmail.com>
2023-02-05 22:25:04 +0330

    app: remove extera lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
-// importing font awsome
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-// importing svg-components
-import { MessageIcon } from "./content/svg-component/social-media/messageIcon";
-// importing pages
-import TestCreateComponentBarnch from "./page/testCreateComponentBarnch";
-import TestCreatePageBranch from "./page/testCreatePageBranch";
 function App() {
   return <div className="App w-100" />;
 }
