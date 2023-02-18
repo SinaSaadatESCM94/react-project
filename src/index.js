67c122d16d5c322b524fae924fd43f5727666ebe
escm94 <escm94.sinasaadat@gmail.com>
2023-02-18 21:59:04 +0330

    index: remove routes and add App

--- src/index.js
+++ src/index.js
@@ -1,33 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.css";
-import Layout from "./layout/layout";
-import Login from "./page/login/login";
-import SignUp from "./page/sign-up/signUp";
-import BoardProject from "./page/designers-board-project/boardProject";
-import NotFound from "./page/not-found";
-import SlideShow from "./page/slide-show";
-import TestCreateComponentBarnch from "./page/testCreateComponentBarnch";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/SinaSaadatESCM94/react-project.git" element={<Layout />}>
-        <Route index path="" element={<Login />} />
-        <Route path="signup" element={<SignUp />} />
-        <Route path="board" element={<BoardProject />} />
-        <Route path="not-found" element={<NotFound />} />
-        <Route path="slide-show" element={<SlideShow />} />
-        <Route path="test-component" element={<TestCreateComponentBarnch />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
