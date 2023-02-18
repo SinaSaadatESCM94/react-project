832d9c760b56723eddaad8378924aa6876322089
escm94 <escm94.sinasaadat@gmail.com>
2023-02-18 21:58:26 +0330

    app: move routers to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import Layout from "./layout/layout";
+import Login from "./page/login/login";
+import SignUp from "./page/sign-up/signUp";
+import BoardProject from "./page/designers-board-project/boardProject";
+import NotFound from "./page/not-found";
+import SlideShow from "./page/slide-show";
+import Calculator from "./page/calculator";
+import TestCreateComponentBarnch from "./page/testCreateComponentBarnch";
 function App() {
-  return <div className="App w-100" />;
+  return (
+    <div className="App w-100">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/SinaSaadatESCM94/react-project.git"
+            element={<Layout />}
+          >
+            <Route index path="" element={<Login />} />
+            <Route path="signup" element={<SignUp />} />
+            <Route path="board" element={<BoardProject />} />
+            <Route path="not-found" element={<NotFound />} />
+            <Route path="slide-show" element={<SlideShow />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route
+              path="test-component"
+              element={<TestCreateComponentBarnch />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
