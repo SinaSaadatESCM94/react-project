f5df4c9de1c300a866c103fe619f5feb8be75dcb
escm94 <escm94.sinasaadat@gmail.com>
2023-02-23 00:52:48 +0330

    index create

---
+++ src/page/index.js
@@ -0,0 +1,23 @@
+import Authentication from "./authentication/authentication";
+import Calculator from "./calculator";
+import BoardProject from "./designers-board-project/boardProject";
+import Login from "./login/login";
+import McuSlides from "./mcu-slide-show";
+import NotFound from "./not-found";
+import SignUp from "./sign-up/signUp";
+import SlideShow from "./slide-show";
+import TestCreateComponentBarnch from "./testCreateComponentBarnch";
+import TestCreatePageBranch from "./testCreatePageBranch";
+
+export {
+  Authentication,
+  Calculator,
+  BoardProject,
+  Login,
+  McuSlides,
+  NotFound,
+  SignUp,
+  SlideShow,
+  TestCreateComponentBarnch,
+  TestCreatePageBranch,
+};
